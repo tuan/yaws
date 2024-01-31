36b8d2c0f9f43fe579908bc804ebb68c614a84bf
Tuan Nguyen <lemonroasted@duck.com>
2024-01-31 09:12:12 -0800

    Allows to match on file name in QuickPick window

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Change Log
 
+## 0.0.4 - 2024-01-31
+
+- Allows to match on file name in QuickPick window.
+
 ## 0.0.3 - 2024-01-31
 
 - Adds icon for the extension
--- src/suggest.ts
+++ src/suggest.ts
@@ -1,4 +1,5 @@
 import * as vscode from "vscode";
+import path from "path";
 import { getCurrentWord, getCurrentWordRange } from "./utils";
 
 export async function showSuggestions(
@@ -16,24 +17,41 @@ export async function showSuggestions(
 
   const regex = new RegExp(`\\b${currentWord}\\w+\\b`, "gi");
 
-  const result = new Set<string>();
+  const result = new Set<vscode.QuickPickItem>();
+  const seen = new Set<string>();
   for (const doc of vscode.workspace.textDocuments) {
     const text = doc.getText();
     const matches = text.match(regex);
     if (matches == null) {
       continue;
     }
-    matches.forEach((match) => result.add(match));
+
+    const fileName = path.basename(doc.fileName);
+    matches.forEach((match) => {
+      if (seen.has(match)) {
+        return;
+      }
+      seen.add(match);
+
+      result.add({
+        label: match,
+        description: fileName,
+      });
+    });
   }
 
   if (result.size === 0) {
     return undefined;
   }
 
-  const picked = await vscode.window.showQuickPick([...result]);
+  const picked = await vscode.window.showQuickPick([...result], {
+    matchOnDescription: true,
+  });
   if (picked == null) {
     return;
   }
 
-  textEditor.edit((builder) => builder.replace(currentWordRange!, picked));
+  textEditor.edit((builder) =>
+    builder.replace(currentWordRange!, picked.label)
+  );
 }
